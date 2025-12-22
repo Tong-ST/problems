6fb908ce610e7282fc0b2d4738fb36c281bda3b5
Tong-ST <145736642+Tong-ST@users.noreply.github.com>
2025-12-22 16:24:48 +0700

    Update_test

--- ps_test/__init__.py
+++ ps_test/__init__.py
@@ -10,7 +10,7 @@ def exists():
 
 @check50.check(exists)
 def test_3_over_4():
-    """input of 3/4 ต้องมี output คือ 75%"""
+    """กรอก input 3/4 ต้องได้ output คือ 75%"""
     input = "3/4"
     output = "75%"
     check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
@@ -18,7 +18,7 @@ def test_3_over_4():
 
 @check50.check(exists)
 def test_round_down():
-    """input of 1/3 yields output of 33%"""
+    """กรอก input 1/3 ต้องได้ output คือ 33%"""
     input = "1/3"
     output = "33%"
     check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
@@ -26,7 +26,7 @@ def test_round_down():
 
 @check50.check(exists)
 def test_round_up():
-    """input of 2/3 yields output of 67%"""
+    """กรอก input 2/3 ต้องได้ output คือ 67%"""
     input = "2/3"
     output = "67%"
     check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
@@ -34,81 +34,52 @@ def test_round_up():
 
 @check50.check(exists)
 def test_empty():
-    """input of 0/100 yields output of E"""
+    """กรอก input 0/100 ต้องได้ output คือ E"""
     input = "0/100"
     output = "E"
     check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
-def test_almost_empty():
-    """input of 1/100 yields output of E"""
-    input = "1/100"
-    output = "E"
-    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
-
-
-@check50.check(exists)
 def test_full():
-    """input of 100/100 yields output of F"""
+    """กรอก input 100/100 ต้องได้ output คือ F"""
     input = "100/100"
     output = "F"
     check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
-@check50.check(exists)
-def test_almost_full():
-    """input of 99/100 yields output of F"""
-    input = "99/100"
-    output = "F"
-    check50.run("python3 fuel.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
-
 
 @check50.check(exists)
 def test_ZeroDivisionError():
-    """input of 100/0 results in reprompt"""
+    """กรอก input 100/0 โปรแกรมต้องให้กรอก input ใหม่"""
     input = "100/0"
     check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
 
 
 @check50.check(exists)
 def test_numerator_greater_than_denominator():
-    """input of 10/3 results in reprompt"""
+    """กรอก input 10/3 โปรแกรมต้องให้กรอก input ใหม่"""
     input = "10/3"
     check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
 
 
 @check50.check(exists)
 def test_str_conversion():
-    """input of three/four results in reprompt"""
+    """กรอก input three/four โปรแกรมต้องให้กรอก input ใหม่"""
     input = "three/four"
     check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
 
 
 @check50.check(exists)
-def test_float_numerator():
-    """input of 1.5/4 results in reprompt"""
+def test_float_numerator(): 
+    """กรอก input 1.5/4 โปรแกรมต้องให้กรอก input ใหม่"""
     input = "1.5/4"
     check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
 
 
 @check50.check(exists)
-def test_float_denominator():
-    """input of 3/5.5 results in reprompt"""
-    input = "3/5.5"
-    check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
-
-
-@check50.check(exists)
-def test_no_slash():
-    """input of 5-10 results in reprompt"""
-    input = "5-10"
-    check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
-
-
-@check50.check(exists)
 def test_negative_fraction():
-    """input of -1/4 results in reprompt"""
+    """กรอก input -1/4 โปรแกรมต้องให้กรอก input ใหม่"""
     input = "-1/4"
     check50.run("python3 fuel.py").stdin(input, prompt=True).reject()
 
